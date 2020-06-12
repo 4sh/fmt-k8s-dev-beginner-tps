3e9f5710fa4b24d8efa2b8573abc8a69b3e3d8a3
Xavier Hanin <xavier.hanin@4sh.fr>
2020-06-12 18:48:56 +0200

    add config loading on srv

--- srv/build.gradle.kts
+++ srv/build.gradle.kts
@@ -5,6 +5,7 @@ val kotlinVersion = "2.0.20"
 val http4kVersion = "5.29.0.0"
 val log4jVersion = "2.12.1"
 val jacksonVersion = "2.10.0"
+val autoKonfigVersion = "1.0.0"
 val jaxbVersion = "2.3.0"
 val kotestVersion = "5.9.1"
 val exposedVersion  = "0.53.0"
@@ -55,6 +56,7 @@ dependencies {
     implementation("com.fasterxml.jackson.core:jackson-core:$jacksonVersion")
     implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:$jacksonVersion")
     implementation("com.fasterxml.jackson.datatype:jackson-datatype-joda:$jacksonVersion")
+    implementation("dev.nohus:AutoKonfig:$autoKonfigVersion")
 
     testImplementation("io.kotest:kotest-runner-junit5-jvm:$kotestVersion") // for kotest framework
     testImplementation("io.kotest:kotest-assertions-core-jvm:$kotestVersion") // for kotest core jvm assertions
