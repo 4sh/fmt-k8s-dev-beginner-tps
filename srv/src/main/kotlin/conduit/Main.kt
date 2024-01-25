2c81dbf0a96bc070eec67a7b118f29483a6f2ba5
Morgan Renou <morgan.renou@4sh.fr>
2024-01-25 11:52:54 +0100

    use images with kustomize

--- srv/src/main/kotlin/conduit/Main.kt
+++ srv/src/main/kotlin/conduit/Main.kt
@@ -35,7 +35,7 @@ fun main(args:Array<String>) {
     val config = conduit.config.auto
     println("db config: ${config.db}")
 
-    val server = startApp("1.0.0-db", config)
+    val server = startApp("2.0.0", config)
     server.block()
 }
 
